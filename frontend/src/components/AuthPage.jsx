40edf1e7166408ee3e62ecf779b6b911634d8be0
Lucas Anjos <lfanjosus@gmail.com>
2023-04-29 14:04:02 -0300

    update url and package.json

--- frontend/src/components/AuthPage.jsx
+++ frontend/src/components/AuthPage.jsx
@@ -5,7 +5,9 @@ const AuthPage = ({ onAuth }) => {
         e.preventDefault();
         const { value } = e.target[0];
         axios
-            .post('http://localhost:3001/authenticate', { username: value })
+            .post('http://lfanjos.github.io/chat-app/authenticate', {
+                username: value,
+            })
             .then((r) => onAuth({ ...r.data, secret: value }))
             .catch((e) => console.log(e));
         onAuth({ username: value, secret: value });
